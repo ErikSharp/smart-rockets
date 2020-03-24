e0d28e440093f63a3bd1fab1307be5ca08d0d12f
CaptainBedpan <mr.erik.sharp@gmail.com>
2020-03-24 22:10:14 +0000

    multiple boxes

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,21 @@ import { BouncyBox } from "./bouncyBox";
 
 import "./style.scss";
 
-const s = (p: any) => {
-    let box: BouncyBox;
+const sketch = (p: p5) => {
+    let boxes: BouncyBox[] = [];
 
     p.setup = function() {
         p.createCanvas(700, 410);
-        box = new BouncyBox();
+        for (let index = 0; index < 10; index++) {
+            boxes[index] = new BouncyBox(p);
+        }
     };
 
     p.draw = function() {
-        box.update();
+        boxes.forEach(b => b.update());
         p.background(0);
-        box.draw();
+        boxes.forEach(b => b.draw());
     };
 };
 
-let myP5 = new p5(s);
+let myP5 = new p5(sketch);
