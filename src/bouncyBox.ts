5cc560d6f641ba4f4515679319604c6fd095ae65
CaptainBedpan <mr.erik.sharp@gmail.com>
2020-03-24 22:09:53 +0000

    made it more interesting

--- src/bouncyBox.ts
+++ src/bouncyBox.ts
@@ -4,16 +4,37 @@ import { updatable } from "./updatable";
 
 export class BouncyBox implements drawable, updatable {
     pos: Vector;
+    vel: Vector;
+    p: p5;
+    color: string;
+    static colors: string[] = ["white", "red", "blue", "green", "magenta"];
 
-    constructor() {
-        this.pos = Vector.random2D();
+    constructor(p: p5) {
+        this.p = p;
+        this.pos = new Vector();
+        this.pos.x = p.width / 2;
+        this.pos.y = p.height / 2;
+
+        this.vel = Vector.random2D();
+        this.vel.setMag(this.p.random(1, 6));
+        this.color = this.p.random(BouncyBox.colors);
     }
 
     update() {
-        console.log("update");
+        if (this.pos.x < 0 || this.pos.x > this.p.width) {
+            this.vel.x *= -1;
+        }
+
+        if (this.pos.y < 0 || this.pos.y > this.p.height) {
+            this.vel.y *= -1;
+        }
+
+        this.pos.add(this.vel);
     }
 
     draw() {
-        console.log("draw");
+        this.p.fill(this.color);
+        this.p.rectMode(this.p.CENTER);
+        this.p.rect(this.pos.x, this.pos.y, 20, 20);
     }
 }
