7fe27dc318554a83e3127e548b20a8ca16bec37f
CaptainBedpan <mr.erik.sharp@gmail.com>
2020-03-25 18:28:53 +0000

    separate source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: "development",
+    //mode: "development",
     entry: {
         app: "./src/index.ts"
     },
-    devtool: "inline-source-map",
+    devtool: "source-map",
     //web server that serves the bundles from memory
     devServer: {
         contentBase: "./dist"
